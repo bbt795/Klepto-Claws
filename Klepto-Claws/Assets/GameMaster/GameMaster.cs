bad9e9087bcf8e143c468a0d232db7585a5b7acc
BananaCat876 <jacq.custer@gmail.com>
2024-04-04 18:03:19 -0400

    Updated NPM and GameMaster code

--- Klepto-Claws/Assets/GameMaster/GameMaster.cs
+++ Klepto-Claws/Assets/GameMaster/GameMaster.cs
@@ -5,10 +5,18 @@ using NETWORK_ENGINE;
 
 public class GameMaster : NetworkComponent
 {
+    public bool GameStarted = false;
     public override void HandleMessage(string flag, string value)
-    {
-        //if flag == "GAMESTART"
-        //   Want to disable PlayerInfo
+    {   
+        if(flag == "GAMESTART")
+        {
+            //   Want to disable PlayerInfo
+            GameStarted = true;
+            foreach(NPM np in GameObject.FindObjectsOfType<NPM>())
+            {
+                np.transform.GetChild(0).gameObject.SetActive(false);
+            }
+        }
     }
     public override void NetworkedStart()
     {
@@ -16,12 +24,32 @@ public class GameMaster : NetworkComponent
     }
     public override IEnumerator SlowUpdate()
     {
+        while(!GameStarted && IsServer)
+        {
+            bool readyGo = true;
+            int count = 0;
+            foreach(NPM np in GameObject.FindObjectsOfType<NPM>())
+            {
+                if(!np.IsReady)
+                {
+                    readyGo = false;
+                    break;
+                }
+                count++;
+            }
+            if(count <1)
+            {
+                readyGo = false;
+            }
+            GameStarted = readyGo;
+            yield return new WaitForSeconds(2);
+        }
         if (IsServer)
         {
             //while(all players have not hit ready)
             //Wait
 
-            //SendUpdate "GAMESTART"
+            SendUpdate("GAMESTART", GameStarted.ToString()); 
 
             //Go to each NetworkPlayerManager and look at their options
             //Create the appropriate character for their options
@@ -30,13 +58,22 @@ public class GameMaster : NetworkComponent
 
             MyId.NotifyDirty();
         }
+        while(IsServer)
+        {
+            if(IsDirty)
+            {
+                SendUpdate("GAMESTART", GameStarted.ToString());
+                IsDirty = false;
+            }
+            yield return new WaitForSeconds(1);
+        }
         yield return new WaitForSeconds(.1f);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        GameStarted = false;
     }
 
     // Update is called once per frame
--- Klepto-Claws/Assets/NetworkPlayerManager/NPM.cs
+++ Klepto-Claws/Assets/NetworkPlayerManager/NPM.cs
@@ -1,22 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using NETWORK_ENGINE;
 
 public class NPM : NetworkComponent
 {
+    public bool IsReady;
     public override void HandleMessage(string flag, string value)
     {
-        throw new System.NotImplementedException();
+        if(flag == "READY")
+        {
+            IsReady = bool.Parse(value);
+            if(IsServer)
+            {
+                SendUpdate("READY", value);
+            }
+        }
+        //throw new System.NotImplementedException();
+    }
+
+    public void UI_Ready(bool r)
+    {
+        if(IsLocalPlayer)
+        {
+            SendCommand("READY", r.ToString());
+        }
     }
 
     public override void NetworkedStart()
     {
-        throw new System.NotImplementedException();
+        if(!IsLocalPlayer)
+        {
+            this.transform.GetChild(0).gameObject.SetActive(false);
+        }
+        //throw new System.NotImplementedException();
     }
 
     public override IEnumerator SlowUpdate()
     {
+        while(IsConnected)
+        {
+            if(IsServer)
+            {
+                if(IsDirty)
+                {
+                    IsDirty = false;
+                }
+            }
+            yield return new WaitForSeconds(.1f);
+        }
         throw new System.NotImplementedException();
     }
 
