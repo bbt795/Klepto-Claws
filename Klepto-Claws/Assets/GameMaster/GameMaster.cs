931453976b2114ba8d2fcb898bbf84b74fe5d441
bbt795 <bubby1031@hotmail.com>
2024-04-04 15:06:56 -0400

    Added Network Player Manager and GameMaster

---
+++ Klepto-Claws/Assets/GameMaster/GameMaster.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NETWORK_ENGINE;
+
+public class GameMaster : NetworkComponent
+{
+    public override void HandleMessage(string flag, string value)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void NetworkedStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override IEnumerator SlowUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
