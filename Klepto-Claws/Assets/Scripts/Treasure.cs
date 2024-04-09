4d738b176d2263cbf8aa3b535cc76543281234be
bbt795 <bubby1031@hotmail.com>
2024-04-08 21:56:08 -0400

    Starting Treasure Implementation

--- Klepto-Claws/Assets/Scripts/Treasure.cs
+++ Klepto-Claws/Assets/Scripts/Treasure.cs
@@ -1,12 +1,49 @@
+using NETWORK_ENGINE;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Treasure : MonoBehaviour
+public class Treasure : NetworkComponent
 {
 
     public int type;
-    public int value;
+    public int treasureValue;
+
+    public override void HandleMessage(string flag, string value)
+    {
+        
+    }
+
+    public override void NetworkedStart()
+    {
+
+        //Generally how we want to assign value
+        switch (this.gameObject.tag)
+        {
+
+            case "Treasure1":
+
+                treasureValue = 250;
+
+                break;
+
+            case "Treasure2":
+
+                treasureValue = 150;
+
+                break;
+
+        }
+
+
+    }
+
+    public override IEnumerator SlowUpdate()
+    {
+
+        yield return new WaitForSeconds(0.1f);
+
+    }
 
     // Start is called before the first frame update
     void Start()
